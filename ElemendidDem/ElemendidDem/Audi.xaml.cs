50e2ba5d2897aa70a7192ab51e60dd709069a9bf
opilane <opilane@HP-CZC6297B89>
2020-10-21 12:26:29 +0300

    Add project files.

---
+++ ElemendidDem/ElemendidDem/Audi.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ElemendidDem
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Audi : ContentPage
+    {
+        public Audi()
+        {
+            BackgroundColor = Color.Wheat;
+            AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+            absoluteLayout.Children.Add(
+                new Label { Text = "Audi", FontSize = 30, TextColor = Color.Black },
+                new Rectangle(175, 20, 100, 60)
+            );
+            Content = absoluteLayout;
+        }
+    }
+}
